0d14c7f03861903719ee0cbf93c112b6679d5e75
akmalrizaev <akmaljon.rizayev@gmail.com>
2024-05-27 08:30:45 +0500

    Pattern Sliding Window

--- 1.Pattern_Sliding_Window/Introduction.js
+++ 1.Pattern_Sliding_Window/Introduction.js
@@ -15,6 +15,7 @@
 // A brute-force algorithm will be to calculate the sum of every 5-element contiguous subarray of the given
 // array and divide the sum by ‘5’ to find the average. This is what the algorithm will look like:
 
+/*
 function find_averages_of_subarrays(K, arr) {
   const result = [];
   for (let i = 0; i < arr.length - K + 1; i++) {
@@ -30,6 +31,45 @@ function find_averages_of_subarrays(K, arr) {
 
 const result = find_averages_of_subarrays(5, [1, 3, 2, 6, -1, 4, 1, 8, 2]);
 console.log(`Averages of subarrays of size K: ${result}`);
+*/
 
 // Output:
 // Average of subarrays of size K: 2.2,2.8,2.4,3.6,2.8
+
+// Time complexity: Since for every element of the input array, we are calculating the sum of its next ‘K’
+// elements, the time complexity of the above algorithm will be O(N∗K)O(N∗K) where ‘N’ is the number of
+// elements in the input array.
+
+// Can we find a better solution? Do we see any inefficiency in the above approach?
+
+// The inefficiency is that for any two consecutive subarrays of size ‘5’, the overlapping part (which will
+// contain four elements) will be evaluated twice.
+
+// The efficient way to solve this problem would be to visualize each contiguous subarray as a sliding
+// window of ‘5’ elements. This means that when we move on to the next subarray, we will slide the
+// window by one element. So, to reuse the sum from the previous subarray, we will subtract the element
+// going out of the window and add the element now being included in the sliding window. This will save us
+// from going through the whole subarray to find the sum and, as a result, the algorithm complexity will reduce to O(N).
+
+// Here is the algorithm for the Sliding Window approach:
+
+function find_averages_of_subarrays(K, arr) {
+  const result = [];
+  let windowSum = 0.0,
+    windowStart = 0;
+  for (let windowEnd = 0; windowEnd < arr.length; windowEnd++) {
+    windowSum += arr[windowEnd]; //add the next element
+    // slide the window, we don't need to slide if we've not hit the required window size of 'K'
+    if (windowEnd >= K - 1) {
+      result.push(windowSum / K); // calculate the average
+      windowSum -= arr[windowStart]; // subtract the element going out
+      windowStart += 1; // slide the window ahead
+    }
+  }
+  return result;
+}
+
+const result = find_averages_of_subarrays(5, [1, 3, 2, 6, -1, 4, 1, 8, 2]);
+console.log(`Averages of subarrays of size K: ${result}`);
+
+// Averages of subarrays of size K: 2.2,2.8,2.4,3.6,2.8
